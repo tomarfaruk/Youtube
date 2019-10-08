3d3da20a7248b55c260d21f8aacce4a3525dfa8a
umorfarukcse@gmail.com <umorfarukcse@gmail.com>
2019-10-09 05:00:18 +0600

    soleve some error

--- Youtube/wsgi.py
+++ Youtube/wsgi.py
@@ -14,8 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Youtube.settings')
 
 application = get_wsgi_application()
-from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(application)
 
 
